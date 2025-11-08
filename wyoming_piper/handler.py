177850f6156adf09630122a2e76be6fe496166fc
Khalob Cognata <khalob@users.noreply.github.com>
2025-11-08 00:43:25 -0500

    Update handler.py

--- wyoming_piper/handler.py
+++ wyoming_piper/handler.py
@@ -145,7 +145,7 @@ class PiperEventHandler(AsyncEventHandler):
         _LOGGER.debug(synthesize)
 
         raw_text = synthesize.text
-        raw_text = regex.sub(r"[^\p{L}\p{N}\s\.\,]", "", raw_text)
+        raw_text = regex.sub(r"[^\p{L}\p{N}\s\.,:-]", "", raw_text)
 
         # Join multiple lines
         text = " ".join(raw_text.strip().splitlines())
